1a0882dc6e61967af88a3823fe8c44f4f6e67537
gazdicdanica <gazdic.sv12.2020@uns.ac.rs>
2024-04-08 15:52:36 +0200

    Initial architecture of project and screen init

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_internship_2024_app/presentation/screens/platforms_screen.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(
+      title: 'Flutter Groceries',
+      theme: ThemeData.dark().copyWith(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color.fromARGB(1, 205, 232, 223),
         ),
+        scaffoldBackgroundColor: Colors.white,
       ),
+      home: const PlatformsScreen(),
     );
   }
 }
---
+++ lib/models/library.dart
@@ -0,0 +1,77 @@
+class Library {
+
+  final int contributionsCount;
+  final int dependentReposCount;
+  final int dependentsCount;
+  final String description;
+  final int forks;
+  final String homepage;
+  final List<String> keywords;
+  final String language;
+  final String latestDownloadUrl;
+  final String latestReleaseNumber;
+
+
+  Library({
+    required this.contributionsCount,
+    required this.dependentReposCount,
+    required this.dependentsCount,
+    required this.description,
+    required this.forks,
+    required this.homepage,
+    required this.keywords,
+    required this.language,
+    required this.latestDownloadUrl,
+    required this.latestReleaseNumber,
+  });
+
+//   {
+//   "contributions_count": 13,
+//   "dependent_repos_count": 20489,
+//   "dependents_count": 57,
+//   "deprecation_reason": null,
+//   "description": "JavaScript Base62 encode/decoder",
+//   "forks": 26,
+//   "homepage": "https://github.com/base62/base62.js",
+//   "keywords": [
+//     "base-62",
+//     "encoder",
+//     "decoder",
+//     "base62",
+//     "encoding",
+//     "javascript"
+//   ],
+//   "language": "JavaScript",
+//   "latest_download_url": "https://registry.npmjs.org/base62/-/base62-2.0.2.tgz",
+//   "latest_release_number": "2.0.2",
+//   "latest_release_published_at": "2024-04-02 11:30:29 UTC",
+//   "latest_stable_release_number": "2.0.2",
+//   "latest_stable_release_published_at": "2024-04-02 11:30:29 UTC",
+//   "license_normalized": false,
+//   "licenses": "MIT",
+//   "name": "base62",
+//   "normalized_licenses": [
+//     "MIT"
+//   ],
+//   "package_manager_url": "https://www.npmjs.com/package/base62",
+//   "platform": "NPM",
+//   "rank": 21,
+//   "repository_license": "MIT",
+//   "repository_status": null,
+//   "repository_url": "https://github.com/base62/base62.js",
+//   "stars": 133,
+//   "status": null,
+//   "versions": [
+//     {
+//       "number": "0.1.0",
+//       "published_at": "2012-02-24 18:04:06 UTC",
+//       "spdx_expression": "NONE",
+//       "original_license": "",
+//       "researched_at": null,
+//       "repository_sources": [
+//         "NPM"
+//       ]
+//     },
+//    ]
+// }
+}
