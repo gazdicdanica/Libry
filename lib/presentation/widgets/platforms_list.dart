4bf89a07af3460994d609c1e8a7251b1edd77af0
gazdicdanica <gazdic.sv12.2020@uns.ac.rs>
2024-04-08 17:07:24 +0200

    Implemented dummy platforms list just to check if everything with bloc works as expected

--- lib/presentation/widgets/platforms_list.dart
+++ lib/presentation/widgets/platforms_list.dart
@@ -1,12 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_internship_2024_app/bloc/bloc/platforms_bloc.dart';
 
-class PlatformsList extends StatelessWidget{
+class PlatformsList extends StatefulWidget{
   const PlatformsList({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+  State<PlatformsList> createState() => _PlatformsListState();
+}
+
+class _PlatformsListState extends State<PlatformsList> {
+
+  @override
+  void initState() {
+    super.initState();
+    context.read<PlatformsBloc>().add(PlatformsRequested());
   }
 
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<PlatformsBloc, PlatformsState>(builder: (context, state){
+      if(state is PlatformsLoading){
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
+      }
+      if(state is PlatformsSuccess){
+        return Center(
+          child: Text(state.platforms),
+        );
+      }
+      if(state is PlatformsFailure){
+        return Center(
+          child: Text(state.errorMessage),
+        );
+      }
+      return const SizedBox();
+    });
+  }
 }
