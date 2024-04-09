13b008831ba629871e93a8d74abb3a9d9bbc1a02
gazdicdanica <gazdic.sv12.2020@uns.ac.rs>
2024-04-09 10:05:28 +0200

    Small changes to card widget

--- lib/presentation/widgets/card_widget.dart
+++ lib/presentation/widgets/card_widget.dart
@@ -11,19 +11,22 @@ class CardWidget extends StatelessWidget {
     return Card(
       margin: const EdgeInsets.symmetric(vertical: 5.0, horizontal: 20.0),
       elevation: 4,
-      child: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 15.0),
-        child: Row(
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20), color: color),
-              width: 8,
-              //probably gonna need to change this height
-              height: 70,
-            ),
-            ...children
-          ],
+      child: InkWell(
+        onTap: () {},
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 15.0),
+          child: Row(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20), color: color),
+                width: 8,
+                //probably gonna need to change this height
+                height: 70,
+              ),
+              ...children
+            ],
+          ),
         ),
       ),
     );
