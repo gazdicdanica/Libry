e27187022168aae55bc3560dc8e4ded6ccb65b62
gazdicdanica <gazdic.sv12.2020@uns.ac.rs>
2024-04-10 16:06:43 +0200

    Added validation and form reset on navigation

--- lib/presentation/widgets/auth_form.dart
+++ lib/presentation/widgets/auth_form.dart
@@ -20,6 +20,14 @@ class _AuthFormState extends State<AuthForm> {
   final _form = GlobalKey<FormState>();
   //TODO: change to true
   var _isLogin = false;
+  final _passwordController = TextEditingController();
+
+  void _authenticate(){
+    final isValid = _form.currentState!.validate();
+    if(!isValid){
+      return;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,9 +38,11 @@ class _AuthFormState extends State<AuthForm> {
           //email
           TextFormField(
             decoration: const InputDecoration(
-                labelText: 'Email',
-                hintText: 'Your email address',
-                border: OutlineInputBorder()),
+              labelText: 'Email',
+              hintText: 'Your email address',
+              border: OutlineInputBorder(),
+              floatingLabelBehavior: FloatingLabelBehavior.always,
+            ),
             keyboardType: TextInputType.emailAddress,
             autocorrect: false,
             textCapitalization: TextCapitalization.none,
@@ -40,7 +50,7 @@ class _AuthFormState extends State<AuthForm> {
               if (value == null ||
                   value.trim().isEmpty ||
                   !EmailValidator.validate(value)) {
-                return 'Email is not in correct format';
+                return 'Email is not in correct format!';
               }
               return null;
             },
@@ -50,49 +60,100 @@ class _AuthFormState extends State<AuthForm> {
           ),
           // password
           TextFormField(
+            controller: _passwordController,
             decoration: const InputDecoration(
-                labelText: 'Password',
-                hintText: 'Your password',
-                border: OutlineInputBorder()),
+              labelText: 'Password',
+              hintText: 'Your password',
+              border: OutlineInputBorder(),
+              floatingLabelBehavior: FloatingLabelBehavior.always,
+            ),
             keyboardType: TextInputType.text,
             autocorrect: false,
             textCapitalization: TextCapitalization.none,
             obscureText: true,
             validator: (value) {
               if (value == null || value.trim().isEmpty || value.length < 6) {
-                return 'Password should contain 6 characters';
+                return 'Password should contain 6 characters!';
               }
               return null;
             },
           ),
+          if (_isLogin)
+            SizedBox(
+              width: double.infinity,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    style: ButtonStyle(
+                        foregroundColor: MaterialStateProperty.all<Color>(
+                            const Color.fromRGBO(0, 166, 141, 1))),
+                    child: const Text('Forgot your password?', style: TextStyle(fontSize: 16, fontWeight: FontWeight.normal),),
+                  ),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                ],
+              ),
+            ),
           if (!_isLogin)
-            const SizedBox(
-              height: 20,
+            Column(
+              children: [
+                const SizedBox(
+                  height: 20,
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: 'Confirm Password',
+                    hintText: 'Repeat your password',
+                    border: OutlineInputBorder(),
+                    floatingLabelBehavior: FloatingLabelBehavior.always,
+                  ),
+                  keyboardType: TextInputType.text,
+                  autocorrect: false,
+                  textCapitalization: TextCapitalization.none,
+                  obscureText: true,
+                  validator: (value) {
+                    if (value == null ||
+                        value.trim().isEmpty ||
+                        value != _passwordController.text) {
+                      return 'Passwords do not match!';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(
+                  height: 40,
+                ),
+              ],
             ),
           // confirm password
-          TextFormField(
-            decoration: const InputDecoration(
-                labelText: 'Confirm Password',
-                hintText: 'Repeat your password',
-                border: OutlineInputBorder()),
-            keyboardType: TextInputType.text,
-            autocorrect: false,
-            textCapitalization: TextCapitalization.none,
-            obscureText: true,
-            validator: (value) {
-              return null;
-            },
+          SizedBox(
+            width: double.infinity,
+            child: ElevatedButton(
+              onPressed: _authenticate,
+              style: buttonStyle,
+              child: Text(_isLogin ? 'Login' : 'Sign up'),
+            ),
           ),
           const SizedBox(
-            height: 40,
+            height: 20,
           ),
           SizedBox(
             width: double.infinity,
             child: ElevatedButton(
-              
-              onPressed: () {},
+              onPressed: () {
+                setState(() {
+                  _form.currentState!.reset();
+                  _isLogin = !_isLogin;
+                });
+              },
               style: buttonStyle,
-              child: Text(_isLogin ? 'Login' : 'Sign up'),
+              child: Text(_isLogin ? 'Sign up' : 'Login'),
             ),
           ),
         ],
