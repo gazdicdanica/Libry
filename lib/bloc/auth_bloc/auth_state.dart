e2c8c5ca7228b3d1cade32a7878ee172b5196f89
gazdicdanica <gazdic.sv12.2020@uns.ac.rs>
2024-04-15 08:45:08 +0200

    Strted refactoring validation to bloc

--- lib/bloc/auth_bloc/auth_state.dart
+++ lib/bloc/auth_bloc/auth_state.dart
@@ -1,18 +1,30 @@
 part of 'auth_bloc.dart';
 
 @immutable
-sealed class AuthState {}
+sealed class AuthState extends Equatable{}
 
-final class AuthInitial extends AuthState {}
+final class AuthInitial extends AuthState {
+  @override
+  List<Object> get props => [];
+}
 
-final class AuthLoading extends AuthState {}
+final class AuthLoading extends AuthState {
+  @override
+  List<Object> get props => [];
+}
 
-final class AuthSuccess extends AuthState {}
+final class AuthSuccess extends AuthState {
+  @override
+  List<Object> get props => [];
+}
 
 final class AuthFailure extends AuthState {
   final String errorMessage;
 
   AuthFailure(this.errorMessage);
+
+  @override
+  List<Object> get props => [errorMessage];
 }
 
 final class AuthEmailFailure extends AuthFailure {
