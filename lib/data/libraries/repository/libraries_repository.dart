66f7e28c8d3c49d19428592c9863edad812b3d18
anna02272 <domonjianna@gmail.com>
2024-04-16 11:29:06 +0200

    Fixed fetching error

--- lib/data/libraries/repository/libraries_repository.dart
+++ lib/data/libraries/repository/libraries_repository.dart
@@ -25,6 +25,10 @@ class LibrariesRepository {
       final List<Library> libraries = List<Library>.from(
           data.map((libraryData) => Library.fromMap(libraryData)).toList());
 
+      if (platformsRepository.platforms.isEmpty) {
+        await platformsRepository.fetchPlatforms();
+      }
+
       final platforms = platformsRepository.platforms;
 
       for (var library in libraries) {
